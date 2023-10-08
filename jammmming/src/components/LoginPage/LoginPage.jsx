79fff06c9dbaf30badc1f37d4504c008cf60bfa5
Timothy Dennis <timothyddennis@gmail.com>
2023-10-08 00:23:47 -0500

    fix: tweaked login page and input fields on app page

--- jammmming/src/components/LoginPage/LoginPage.jsx
+++ jammmming/src/components/LoginPage/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import url from "../../functions/apiAuthorize";
+import "./LoginPage.css";
 
 function LoginPage() {
     const handleClick = () => {
@@ -7,7 +8,15 @@ function LoginPage() {
     }
 
     return (
-        <div>
+        <div id="login">
+            <h1>Welcome to Mr. Lister!</h1>
+            <p>
+                Mr. Lister is a web app that allows the user to create a playlist and save it to 
+                their Spotify account. This application is built in React, and queries the Spotify 
+                API for track search and playlist creation. It also uses React Router to create separate 
+                routes for this page and for the application, and Axios is used for handling GET and 
+                POST requests to the Spotify API.
+            </p>
             <button onClick={handleClick}>Login with Spotify</button>
         </div>
     )
